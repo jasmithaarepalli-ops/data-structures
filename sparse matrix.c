7d85f6b935fda4b3a89a6dd866560eb38eff4243
jasmithaarepalli-ops <jasmithaarepalli@gmail.com>
2026-04-15 08:37:55 +0530

    Add files via upload

---
+++ sparse matrix.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main() {
+    int matrix[10][10], sparse[20][3];
+    int i, j, rows, cols, k = 1;
+
+    printf("Enter rows and columns: ");
+    scanf("%d %d", &rows, &cols);
+
+    printf("Enter matrix elements:\n");
+
+    // Input matrix
+    for(i = 0; i < rows; i++) {
+        for(j = 0; j < cols; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    // Count non-zero elements and store in sparse matrix
+    for(i = 0; i < rows; i++) {
+        for(j = 0; j < cols; j++) {
+            if(matrix[i][j] != 0) {
+                sparse[k][0] = i;
+                sparse[k][1] = j;
+                sparse[k][2] = matrix[i][j];
+                k++;
+            }
+        }
+    }
+
+    // First row stores size and count
+    sparse[0][0] = rows;
+    sparse[0][1] = cols;
+    sparse[0][2] = k - 1;
+
+    // Display sparse matrix
+    printf("\nSparse Matrix Representation:\n");
+    printf("Row Col Value\n");
+
+    for(i = 0; i < k; i++) {
+        printf("%d   %d   %d\n", sparse[i][0], sparse[i][1], sparse[i][2]);
+    }
+
+    return 0;
+}
